fe089de9b47ec89a4e09c69d638b4ffd06655b3d
SkhundinMichail <113530482+SkhundinMichail@users.noreply.github.com>
2022-10-30 11:15:06 +0300

    30.1022

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function openForm() {
+  ("myForm").style.display = "flex";
+}
+function openForm() {
+  if ((myForm).style.display = "flex") {
+    (button).style.display = "none";
+  }
+}
+function closeForm() {
+  ("myForm").style.display = "none";
+}
+function closeForm() {
+  if ((myForm).style.display = "none") {
+    (button).style.display = "flex";
+  }
+}
+
+
